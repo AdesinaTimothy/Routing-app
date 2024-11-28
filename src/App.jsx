295a4ad889014950638bce31307b1970c59734c9
Timothy Adesina <timobest121@gmail.com>
2024-11-28 18:50:54 +0100

    Initial boy

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import About from "./About.jsx"
+import Home from "./Home.jsx"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import Services from "./Services.jsx"
+import RootLayout from "./RouteLayout.jsx"
+
+function App() {
+ 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route
+        index
+        element={<Home />}
+      />
+
+      <Route
+        path="/about"
+        element={<About />}
+      />
+
+      <Route
+        path="/services"
+        element={<Services />}
+      />
+
+
+    </Route>
+  ))
+
+  return (
+    <RouterProvider 
+    router = {router} />
+  )
+}
+
+export default App
